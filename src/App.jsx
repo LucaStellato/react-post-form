84b372cbb1655d842c2ccda8ba5a3d70869cc375
LucaStellato <lucastellato435@gmail.com>
2025-11-13 13:57:07 +0100

    add rafio button and other bug...

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ function App() {
   const [formData, setFormData] = useState({
     author: '',
     title: '',
-    body: ''
+    body: '',
+    public: 'true'
 
 
   });
@@ -45,6 +46,12 @@ function App() {
       <div className="mb-3">
         <input type="text" className="form-control" name='body' placeholder='Inserisci il testo del post' value={formData.body} onChange={handleFormData} />
       </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" name='public' checked={formData.public} />
+        <label className="form-check-label">post pubblico</label>
+
+      </div>
+
 
       <button type="submit" className="btn btn-primary">Submit</button>
     </form>
